93c399e93a6ca8f8aed0b0b22b3697c3c68f41f7
itzdanish950 <Practice>
2026-05-18 15:38:02 +0530

    utils code

--- Readme.md
+++ Readme.md
@@ -3,3 +3,9 @@
 Create fullStack Project
 
 -[model link] (https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj)
+
+# Connect .env for import Process
+dotenv.config({
+    path:"./env";
+})
+-r dotenv/config --experimental-json-modules
---
+++ src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError{
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = false
+        this.errors = errors
+
+        if(stach){
+            this.stack = stack
+        } else {
+            error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export { ApiError }
